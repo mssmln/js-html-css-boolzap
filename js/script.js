07d4e4078c8acdc995edff85d3ca225d264d32fd
Massimiliano <m.croci.ly@gmail.com>
2021-02-02 14:46:01 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-// Milestone 1
+// Milestone 1 --> completed
 // ● Replica della grafica c on l a possibilità di avere messaggi scritti dall’utente (verdi) e
 // dall’interlocutore (bianco) assegnando due classi CSS diverse
 // ● Visualizzazione dinamica della lista contatti: tramite l a direttiva v-for, visualizzare nome e immagine di ogni contatto
+// Milestone 2
+// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra la conversazione del contatto cliccato
 
 var app = new Vue ({
   el : '#app',
@@ -13,19 +17,19 @@ var app = new Vue ({
         visible: true,
         messages: [
           {
-          date: '10/01/2020 15:30:55',
-          text: 'Hai portato a spasso il cane?',
-          status: 'sent'
+            date: '10/01/2020 15:30:55',
+            text: 'Hai portato a spasso il cane?',
+            status: 'sent'
           },
           {
-          date: '10/01/2020 15:50:00',
-          text: 'Ricordati di dargli da mangiare',
-          status: 'sent'
+            date: '10/01/2020 15:50:00',
+            text: 'Ricordati di dargli da mangiare',
+            status: 'sent'
           },
           {
-          date: '10/01/2020 16:15:22',
-          text: 'Tutto fatto!',
-          status: 'received'
+            date: '10/01/2020 16:15:22',
+            text: 'Tutto fatto!',
+            status: 'received'
           }
         ],
       },
@@ -35,19 +39,19 @@ var app = new Vue ({
         visible: true,
         messages: [
           {
-          date: '20/03/2020 16:30:00',
-          text: 'Ciao come stai?',
-          status: 'sent'
+            date: '20/03/2020 16:30:00',
+            text: 'Ciao come stai?',
+            status: 'sent'
           },
           {
-          date: '20/03/2020 16:30:55',
-          text: 'Bene grazie! Stasera ci vediamo?',
-          status: 'received'
+            date: '20/03/2020 16:30:55',
+            text: 'Bene grazie! Stasera ci vediamo?',
+            status: 'received'
           },
           {
-          date: '20/03/2020 16:35:00',
-          text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-          status: 'sent'
+            date: '20/03/2020 16:35:00',
+            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+            status: 'sent'
           }
         ],
       },
@@ -92,5 +96,10 @@ var app = new Vue ({
       },
     ]
   },
+  methods : {
+    showChat(){
+      alert('test')
+    }
+  }
 
 });
